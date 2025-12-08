d6503bc15bd299c860428005e67d10bf237fad2e
Kelsen <kelsen.brito@gmail.com>
2025-12-07 21:50:42 -0300

    Módulo de pacotes finalizado

---
+++ go.mod
@@ -0,0 +1,5 @@
+module curso-basic-golang
+
+go 1.25.1
+
+require github.com/K77-dev/golang v0.0.0-20251208003802-059a1aaa70ff
---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/K77-dev/golang"
+)
+
+func main() {
+	fmt.Println(golang.Circ(6.0))
+	fmt.Println(golang.Rect(5.0, 2.0))
+	// fmt.Println(golang._TrianguloEq(5.0, 2.0))
+}
